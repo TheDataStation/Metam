ba81f82f3e1c45ae00d9a488684d9004c11c825f
Jportet <javierportet@gmail.com>
2022-04-04 17:05:11 -0500

    basic structure

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Metam</h1>
+
+      <input type="file" id="myFile" name="filename" />
+
+    </div>
+  );
+}
+
+export default App;
