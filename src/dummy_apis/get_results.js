c647f5f19b7bd58776fd352ad0992b0a7e0aca10
Jportet <javierportet@gmail.com>
2022-04-04 17:05:22 -0500

    dummy api functions

---
+++ src/dummy_apis/get_results.js
@@ -0,0 +1,9 @@
+const get_results = () => {
+
+    return [
+        {name: "table1", score: 1, recommended: false },
+        {name: "table2", score: 4, recommended: true },
+        {name: "table3", score: 2, recommended: false },
+    ]
+    
+}
---
+++ src/dummy_apis/get_tables.js
@@ -0,0 +1,9 @@
+const get_tables = () => {
+
+    return [
+        {name: "table1" },
+        {name: "table2" },
+        {name: "table3" },
+    ]
+
+}
