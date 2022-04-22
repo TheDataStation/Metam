e950707345acfde28a069bc3aca34bd40b1e93c7
JPortet <javierportet@gmail.com>
2022-04-22 00:15:22 -0500

    Delete ResultCard

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import ResultCard from '../../components/ResultCard'
 import TableCard from '../../components/TableCard'
 import get_results from '../../dummy_apis/get_results'
 
