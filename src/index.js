20a1b8be9d5ca2ca1865488524cd14ec6d32a4b9
JPortet <javierportet@gmail.com>
2022-04-22 20:05:33 -0500

    refactor App component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Input from './pages/Input'
+import Results from './pages/Results'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<Input />} />
+      <Route path="/results" element={<Results />} />
+    </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
